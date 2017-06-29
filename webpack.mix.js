afd3e9064a4183fc8d523d9624ad0e2c281b885d
Pepe Garcia <pgarciag93@gmail.com>
2017-06-29 21:37:11 +0200

    try

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,9 +14,9 @@ let mix = require('laravel-mix');
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
 
-mix.copy('/tmp/build_ef686f7243dd8e263537a5c2bbae0438/node_modules/bootstrap-switch/dist/js/bootstrap-switch.js', 'public/js');
-mix.copy('/tmp/build_ef686f7243dd8e263537a5c2bbae0438/node_modules/bootstrap-switch/dist/css/bootstrap3/bootstrap-switch.css', 'public/css');
-mix.copy('/tmp/build_ef686f7243dd8e263537a5c2bbae0438/node_modules/animate.css/animate.css', 'public/css');
+mix.copy('node_modules/bootstrap-switch/dist/js/bootstrap-switch.js', 'public/js');
+mix.copy('node_modules/bootstrap-switch/dist/css/bootstrap3/bootstrap-switch.css', 'public/css');
+mix.copy('node_modules/animate.css/animate.css', 'public/css');
 
-mix.copy('/tmp/build_ef686f7243dd8e263537a5c2bbae0438/node_modules/sweetalert/dist/sweetalert.css', 'public/css');
-mix.copy('/tmp/build_ef686f7243dd8e263537a5c2bbae0438/node_modules/sweetalert/dist/sweetalert.min.js', 'public/js');
+mix.copy('node_modules/sweetalert/dist/sweetalert.css', 'public/css');
+mix.copy('node_modules/sweetalert/dist/sweetalert.min.js', 'public/js');
