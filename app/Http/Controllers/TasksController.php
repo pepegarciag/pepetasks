bcf2b865547b202d45b4aa3065c9aa568f1dfe11
Pepe Garcia <pgarciag93@gmail.com>
2017-07-02 14:41:52 +0200

    Redirect

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -42,7 +42,7 @@ class TasksController extends Controller
         $task->active = TRUE;
         $task->save();
 
-        return back();
+        return redirect('home');
     }
 
     public function get(Request $request, Task $task)
